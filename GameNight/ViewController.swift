d01b07ba8aebc0ce95594c69524f41efb4980379
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-07-17 09:57:38 -0600

    First Commit

---
+++ GameNight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameNight
+//
+//  Created by Timothy Rosenvall on 7/17/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
