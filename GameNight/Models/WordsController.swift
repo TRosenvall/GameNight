262911a0ad193e1303be4ea68848de9c735127ad
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-07-18 15:22:54 -0600

    Model and Model Controller

--- GameNight/Models/WordsController.swift
+++ GameNight/Models/WordsController.swift
@@ -13,7 +13,88 @@ class WordsController {
     // singleton
     static let shared = WordsController()
     
-    // source of truth
-    var words: [Words]?
+    // Source Of Truth
+    var word: String = ""
     
+    //CRUD
+    
+    // Create
+    func createRandomWord() {
+        
+        // generate randomn number
+        let randomNumber = Int.random(in: 0...WordHelper.wordBank.count-1)
+        
+        let chosenWord = WordHelper.wordBank[randomNumber]
+        word = chosenWord
+    }
+    
+    func createHangManTemplate() -> String {
+        
+        var hangmanWord = ""
+        
+        let chosenWord = self.word
+        
+        for _ in chosenWord {
+            
+            hangmanWord += "_"
+        }
+        return hangmanWord
+    }
+    
+    // Update
+    func updateHangManWord(letter: Character) -> String {
+        
+        let chosenWord = self.word
+        var hangmanWordUpdated = self.createHangManTemplate()
+        
+        if chosenWord.contains(letter) {
+            
+            // Array of string positions
+            var indexArray: [String.Index] = []
+            // Starting index
+            var index: Int = 0
+            // Loop through string
+            for char in chosenWord {
+                
+                // init a string.index from the current index property
+                // what index of the string am we at?
+                let stringIndex = String.Index(utf16Offset: index, in: chosenWord)
+                
+                // increment the index property
+                index += 1
+                // compare the letter to the character we are at in the loop
+                if letter == char {
+                    // position of letter
+                    // append the character's position to our string position array
+                    indexArray.append(stringIndex)
+                }
+            }
+            
+            // loop through our string position array
+            for index in indexArray {
+                // insert the word at that position
+                hangmanWordUpdated.insert(letter, at: index)
+                // remove the offset _ from the string.
+                hangmanWordUpdated.remove(at: hangmanWordUpdated.index(after: index))
+            }
+        }
+        return hangmanWordUpdated
+    }
+    
+    func createCorrectAnswerFeedback() -> String {
+        // generate random number
+        let randomNumber = Int.random(in: 0...FeedbackHelper.correct.count-1)
+        
+        let correctFeedback = FeedbackHelper.correct[randomNumber]
+        return correctFeedback
+    }
+    
+    func createWrongAnswerFeedback() -> String {
+        // generate random number
+        let randomNumber = Int.random(in: 0...FeedbackHelper.wrong.count-1)
+        
+        let wrongFeedback = FeedbackHelper.wrong[randomNumber]
+        return wrongFeedback
+    }
 }
+
