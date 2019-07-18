cafa0dd266d1151f44a82bdb72d509db23521466
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-07-18 13:46:51 -0600

    Finished Gameplay Storyboard

---
+++ GameNight/Controllers/ViewControllers/PlayGameViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PlayGameViewController.swift
+//  GameNight
+//
+//  Created by Timothy Rosenvall on 7/18/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import UIKit
+
+class PlayGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension PlayGameViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
