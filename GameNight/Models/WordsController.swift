89ca9860e80667082281d9827e076d0f55e82ed6
Julia Rodriguez <juliarodriguez@Julias-MacBook-Pro.local>
2019-07-18 14:35:01 -0600

    clean

--- GameNight/Models/WordsController.swift
+++ GameNight/Models/WordsController.swift
@@ -42,5 +42,59 @@ class WordsController {
         let wrongFeedback = FeedbackHelper.wrong[randomNumber]
         return wrongFeedback
     }
+    func createHangManTemplate() -> String {
+        
+        var hangmanWord = ""
+        
+        let chosenWord = self.createRandomWord()
+        
+        for char in chosenWord {
+            
+            hangmanWord += "_"
+        }
+        return hangmanWord
+    }
     
+    // Update
+    func updateHangManWord() -> String {
+        
+        let chosenWord = self.createRandomWord()
+        var hangmanWordUpdated = self.createHangManTemplate()
+        
+        func fillInWord(letter: Character) {
+            
+            if chosenWord.contains(letter) {
+                
+                // Array of string positions
+                var indexArray: [String.Index] = []
+                // Starting index
+                var index: Int = 0
+                // Loop through string
+                for char in chosenWord {
+                    
+                    // init a string.index from the current index property
+                    // what index of the string am we at?
+                    let stringIndex = String.Index(utf16Offset: index, in: chosenWord)
+                    
+                    // increment the index property
+                    index += 1
+                    // compare the letter to the character we are at in the loop
+                    if letter == char {
+                        // position of letter
+                        // append the character's position to our string position array
+                        indexArray.append(stringIndex)
+                    }
+                }
+            
+                // loop through our string position array
+                for index in indexArray {
+                    // insert the word at that position
+                    hangmanWordUpdated.insert(letter, at: index)
+                    // remove the offset _ from the string.
+                    hangmanWordUpdated.remove(at: hangmanWordUpdated.index(after: index))
+                }
+            }
+        }
+        return hangmanWordUpdated
+    }
 }
